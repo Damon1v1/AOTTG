2f2b22703ff2d1fc1ac0840e70433c54a531eb62
Damon Burda <damonburda96@gmail.com>
2022-04-25 13:39:52 -0700

    styled navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
 
 
 function Header() {
         return(
           <div>
-            <Navbar bg="dark" variant="dark">
-            <Container>
-              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Game</Nav.Link>
-                <Nav.Link href="#pricing">About</Nav.Link>
+            <Navbar className='custom-Navbar'>
+            <Container id="header">
+              <Nav className='ms-auto'>
+                <Nav.Link id='link' href="#home">Home</Nav.Link>
+                <Nav.Link id='link' href="#game">Game</Nav.Link>
+                <Nav.Link id='link' href="#about">About</Nav.Link>
               </Nav>
             </Container>
           </Navbar>
